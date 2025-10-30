4af056bcacbc6d27de29e75bddd6871defa27a1b
Szervinsk <mathszer1103@gmail.com>
2025-10-30 19:19:33 -0300

    [feat] Implementação da verificação por balanceamento de parênteses

--- src/main/validador.js
+++ src/main/validador.js
@@ -1,26 +1,52 @@
 function ehExpressaoValida(expressao) {
-    const operador = ['+', '-', '*', '/']
-    const isOperador = (char) => operador.includes(char);
-    const primeiroCaractere = expressao[0];
-    const ultimoCaractere = expressao[expressao.length - 1];
+  const operadores = ["+", "-", "*", "/"];
 
-    if (isOperador(primeiroCaractere) && primeiroCaractere !== '-') {
+  // função que verifica se o caractere é um operador
+  const isOperador = (char) => operadores.includes(char);
+
+  // verifica o primeiro e o último caractere
+  const primeiroCaractere = expressao[0];
+  const ultimoCaractere = expressao[expressao.length - 1];
+
+  if (isOperador(primeiroCaractere) && primeiroCaractere !== "-") {
+    return false;
+  }
+  if (isOperador(ultimoCaractere)) {
+    return false;
+  }
+
+  // verifica operadores consecutivos
+  for (let i = 0; i < expressao.length - 1; i++) {
+    const charAtual = expressao[i];
+    const charProximo = expressao[i + 1];
+
+    if (isOperador(charAtual)) {
+      if (isOperador(charProximo)) {
         return false;
+      }
     }
-    if (isOperador(ultimoCaractere)) {
-        return false;
-    }
+  }
 
-    for (let i = 0; i < expressao.length - 1; i++) {
-        const charAtual = expressao[i];
-        const charProximo = expressao[i + 1];
-
-        if (isOperador(charAtual)) {
-            if (isOperador(charProximo)) {
-                return false;
-            }
-        }
+  // verifica parênteses balanceados
+  let contadorParenteses = 0;
+  for (let char of expressao) {
+    if (char === "(") {
+      contadorParenteses++;
+    } else if (char === ")") {
+      contadorParenteses--;
     }
 
-    return true;
-} module.exports = ehExpressaoValida;
+    // se o contador for negativo retornamos falso e interrompemos o loop
+    if (contadorParenteses < 0) {
+      return false;
+    }
+  }
+
+  // se o contador de parentes não for igual a zero, a expressão não está balanceada
+  if (contadorParenteses !== 0) {
+    return false;
+  }
+
+  return true;
+}
+module.exports = ehExpressaoValida;
--- src/test/validador.test.js
+++ src/test/validador.test.js
@@ -1,7 +1,6 @@
 const  ehExpressaoValida = require('../main/validador.js');
 
-
-
+// testes para começo e fim com operadores
 test('Começo com operador', ()=>{
     expect( ehExpressaoValida("+1")).toBe(false);
 });
@@ -12,6 +11,7 @@ test('Com operador corrrecleato',()=>{
     expect(ehExpressaoValida('1+2')).toBe(true);
 });
 
+// testes para operadores consecutivos
 test('Dois operadores iguais seguidos', () => {
     expect(ehExpressaoValida('1--2')).toBe(false);
 });
@@ -23,3 +23,17 @@ test('Dois operadores diferentes seguidos', () => {
 test('Três operadores seguidos', () => {
     expect(ehExpressaoValida('1+-*2')).toBe(false);
 });
+
+// testes para parênteses 
+test("Parênteses balanceados", () => {
+    expect(ehExpressaoValida('(1+2)*(3-4)')).toBe(true);
+})
+
+test("Parênteses não balanceados - mais abertos", () => {
+    expect(ehExpressaoValida('(((1+2) * 3-4)')).toBe(false);
+})
+
+test("Parênteses não balanceados - mais fechados", () => {
+    expect(ehExpressaoValida('(1+2)) * (3-4)')).toBe(false);
+});
+
